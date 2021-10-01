bfd0f3147d29780ac36e5be58db390a8d84a5386
bias <admin@rangsdorf.de>
2021-10-01 09:24:07 +0200

    Add support to pass credentials to wmi-query

--- Get-Monitor.psm1
+++ Get-Monitor.psm1
@@ -18,8 +18,8 @@ Function Get-Monitor {
 
       Manufacturer Model    SerialNumber AttachedComputer
       ------------ -----    ------------ ----------------
-      HP           HP E241i CN12345678   SSL1-F1102-1G2Z 
-      HP           HP E241i CN91234567   SSL1-F1102-1G2Z 
+      HP           HP E241i CN12345678   SSL1-F1102-1G2Z
+      HP           HP E241i CN91234567   SSL1-F1102-1G2Z
       HP           HP E241i CN89123456   SSL1-F1102-1G2Z
 
       .EXAMPLE
@@ -29,8 +29,8 @@ Function Get-Monitor {
       Manufacturer Model      SerialNumber AttachedComputer
       ------------ -----      ------------ ----------------
       HP           HP LA2405x CN12345678   SSL7-F108F-9D4Z
-      HP           HP E241i   CN91234567   SSL1-F1102-1G2Z 
-      HP           HP E241i   CN89123456   SSL1-F1102-1G2Z 
+      HP           HP E241i   CN91234567   SSL1-F1102-1G2Z
+      HP           HP E241i   CN89123456   SSL1-F1102-1G2Z
       HP           HP E241i   CN78912345   SSL1-F1102-1G2Z
       HP           HP ZR22w   CN67891234   SSA7-F1071-0T7F
 
@@ -39,12 +39,14 @@ Function Get-Monitor {
 
   [CmdletBinding()]
   PARAM (
-    [Parameter(ValueFromPipeline=$True,ValueFromPipelineByPropertyName=$True)]
-    [String[]]$ComputerName = $env:ComputerName
+    [Parameter(ValueFromPipeline = $True, ValueFromPipelineByPropertyName = $True)]
+    [String[]]$ComputerName = $env:ComputerName,
+    [Parameter(Mandatory = $false)]
+    [PSCredential]$Credential
   )
-  
+
   #List of Manufacture Codes that could be pulled from WMI and their respective full names. Used for translating later down.
-  $ManufacturerHash = @{ 
+  $ManufacturerHash = @{
     "AAC" =	"AcerView";
     "ACR" = "Acer";
     "AOC" = "AOC";
@@ -83,7 +85,7 @@ Function Get-Monitor {
     "LEN" = "Lenovo";
     "LGD" = "Asus";
     "LPL" = "Fujitsu";
-    "MAX" = "Belinea"; 
+    "MAX" = "Belinea";
     "MEI" = "Panasonic";
     "MEL" = "Mitsubishi Electronics";
     "MS_" = "Panasonic";
@@ -109,41 +111,42 @@ Function Get-Monitor {
     "ZCM" = "Zenith";
     "UNK" = "Unknown";
     "_YV" = "Fujitsu";
-      }
-      
-  
+  }
+
+
   #Takes each computer specified and runs the following code:
   ForEach ($Computer in $ComputerName) {
-  
+
     #Grabs the Monitor objects from WMI
     $Monitors = Get-WmiObject -Namespace "root\WMI" -Class "WMIMonitorID" -ComputerName $Computer -ErrorAction SilentlyContinue
-    
+
     #Creates an empty array to hold the data
     $Monitor_Array = @()
-    
-    
+
+
     #Takes each monitor object found and runs the following code:
     ForEach ($Monitor in $Monitors) {
-      
+
       #Grabs respective data and converts it from ASCII encoding and removes any trailing ASCII null values
       If ([System.Text.Encoding]::ASCII.GetString($Monitor.UserFriendlyName) -ne $null) {
-        $Mon_Model = ([System.Text.Encoding]::ASCII.GetString($Monitor.UserFriendlyName)).Replace("$([char]0x0000)","")
-      } else {
+        $Mon_Model = ([System.Text.Encoding]::ASCII.GetString($Monitor.UserFriendlyName)).Replace("$([char]0x0000)", "")
+      }
+      else {
         $Mon_Model = $null
       }
-      $Mon_Serial_Number = ([System.Text.Encoding]::ASCII.GetString($Monitor.SerialNumberID)).Replace("$([char]0x0000)","")
-      $Mon_Attached_Computer = ($Monitor.PSComputerName).Replace("$([char]0x0000)","")
-      $Mon_Manufacturer = ([System.Text.Encoding]::ASCII.GetString($Monitor.ManufacturerName)).Replace("$([char]0x0000)","")
-      
+      $Mon_Serial_Number = ([System.Text.Encoding]::ASCII.GetString($Monitor.SerialNumberID)).Replace("$([char]0x0000)", "")
+      $Mon_Attached_Computer = ($Monitor.PSComputerName).Replace("$([char]0x0000)", "")
+      $Mon_Manufacturer = ([System.Text.Encoding]::ASCII.GetString($Monitor.ManufacturerName)).Replace("$([char]0x0000)", "")
+
       #Filters out "non monitors". Place any of your own filters here. These two are all-in-one computers with built in displays. I don't need the info from these.
-      If ($Mon_Model -like "*800 AIO*" -or $Mon_Model -like "*8300 AiO*") {Break}
-      
+      If ($Mon_Model -like "*800 AIO*" -or $Mon_Model -like "*8300 AiO*") { Break }
+
       #Sets a friendly name based on the hash table above. If no entry found sets it to the original 3 character code
       $Mon_Manufacturer_Friendly = $ManufacturerHash.$Mon_Manufacturer
       If ($Mon_Manufacturer_Friendly -eq $null) {
         $Mon_Manufacturer_Friendly = $Mon_Manufacturer
       }
-      
+
       #Creates a custom monitor object and fills it with 4 NoteProperty members and the respective data
       $Monitor_Obj = [PSCustomObject]@{
         Manufacturer     = $Mon_Manufacturer_Friendly
@@ -151,16 +154,16 @@ Function Get-Monitor {
         SerialNumber     = $Mon_Serial_Number
         AttachedComputer = $Mon_Attached_Computer
       }
-      
+
       #Appends the object to the array
       $Monitor_Array += $Monitor_Obj
 
     } #End ForEach Monitor
-  
+
     #Outputs the Array
     $Monitor_Array
-    
-} #End ForEach Computer
+
+  } #End ForEach Computer
 
 
 }
